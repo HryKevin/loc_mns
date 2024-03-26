ef0031a87c60529bbf98ce07df9418f684a62fdc
HryKevin <138463362+HryKevin@users.noreply.github.com>
2024-03-26 10:41:33 +0100

    Lang fr

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?? '' ?></title>
+    <title>
+        <?= $title ?? '' ?>
+    </title>
     <meta name="description" content="<?= $description ?? '' ?>">
 </head>
+
 <body>
 
     <main>
@@ -13,4 +17,5 @@
     </main>
 
 </body>
+
 </html>
