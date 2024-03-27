fa4020bc4e96771009e7666b4eee0e6845011275
alexialang <alexialang2@gmail.com>
2024-03-27 16:24:59 +0100

    test merge connexion

---
+++ templates/connexion.html.php
@@ -0,0 +1,23 @@
+<div class="container-connexion">
+  <div class="img-connexion">
+    <img src="assets/img/connexion.svg" alt="connexion">
+    <img class="logo-connexion" src="assets/img/logo.svg" alt="logo">
+  </div>
+  <div class="container-right-connexion">
+    <div class="container-form-connexion">
+      <h1>SE CONNECTER</h1>
+      <form>
+        <div><label>Email : </label>
+          <input type="email" placeholder="  Email" />
+        </div>
+        <div><label>Mot de passe : </label>
+          <input type="password" placeholder="  Mot de passe" />
+        </div>
+        <a class="mdp" href="/">Mot de passe oublié ?</a>
+        <div><input class="button" type="submit" value="Connexion" /></div>
+      </form>
+      <a href="/>">Vous n'êtes pas encore inscrit ?</a>
+    </div>
+  </div>
+
+</div>
