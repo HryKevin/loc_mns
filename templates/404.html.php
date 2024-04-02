c703eb720cbea0bc8065160caa27f3f4fac3467c
alexialang <alexialang2@gmail.com>
2024-04-02 16:09:50 +0200

    error 404 page

--- templates/404.html.php
+++ templates/404.html.php
@@ -1,1 +1,19 @@
-<h1>Erreur 404</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <title>404</title>
+</head>
+
+<body>
+  <div class="container-404">
+
+    <img src="assets/img/error404.svg">
+    <h1>Erreur 404</h1>
+  </div>
+</body>
+
+</html>
