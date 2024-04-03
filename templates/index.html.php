4d3aee920619880aba55240a3ca54c401216dab4
alexialang <alexialang2@gmail.com>
2024-04-03 16:57:07 +0200

    index push

--- templates/index.html.php
+++ templates/index.html.php
@@ -1,27 +1,30 @@
 <div class="container-index">
   <div class="left-section-index">
     <!-- Menu Latéral -->
-    <div class="logo-section-sidebar">
-      <img src="assets/img/logo_computer_locmns.svg" alt="logo-computer" />
-      <img src="assets/img/logo_locmns.svg" alt="logo" />
+    <div>
+      <div class="logo-section-sidebar">
+        <img src="assets/img/logo_computer_locmns.svg" alt="logo-computer" />
+        <img src="assets/img/logo_locmns.svg" alt="logo" />
+      </div>
+      <ul>
+        <li><a href="/"> <img src="assets/img/house.svg" />Accueil</a></li>
+        <li><a href="/"> <img src="assets/img/computer.svg" />Matériels</a></li>
+        <li><a href="/"> <img src="assets/img/bell.svg" />Notifications</a></li>
+        <li><a href="/"> <img src="assets/img/users.svg" />Utilisateurs</a></li>
+        <li><a href="/"> <img src="assets/img/gear.svg" />Réglages</a></li>
+      </ul>
     </div>
-    <ul>
-      <li><a href="/"> <img src="assets/img/house.svg" />Accueil</a></li>
-      <li><a href="/"> <img src="assets/img/computer.svg" />Matériels</a></li>
-      <li><a href="/"> <img src="assets/img/bell.svg" />Notifications</a></li>
-      <li><a href="/"> <img src="assets/img/users.svg" />Utilisateurs</a></li>
-      <li><a href="/"> <img src="assets/img/gear.svg" />Réglages</a></li>
-    </ul>
     <div><a href="/"><img src="assets/img/door.svg" />
         Déconnexion</a>
     </div>
   </div>
-  <div class=rigth-section-index>
+  <div class="rigth-section-index">
     <!-- Nav -->
     <div class="nav-bar">
       <h1>Accueil</h1>
       <div>
-        <input type=text />
+        <input class="input-search" type=text />
+        <img class="search" src="assets/img/magnifier.svg" />
         <img src="assets/img/user.svg" />
         <img src="assets/img/chevron-down.svg" />
       </div>
