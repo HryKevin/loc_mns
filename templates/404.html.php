d02ea6de46ac27a95746afaf07f6d8c8795932bb
alexialang <alexialang2@gmail.com>
2024-04-03 13:43:04 +0200

    404 rectification

--- templates/404.html.php
+++ templates/404.html.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="container-404">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/styles.css">
-  <title>404</title>
-</head>
-
-<body>
-  <div class="container-404">
-
-    <img src="assets/img/error404.svg">
-    <h1>Erreur 404</h1>
-  </div>
+  <img src="assets/img/error404.svg">
+  <h1>Erreur 404</h1>
+</div>
 </body>
-
-</html>
