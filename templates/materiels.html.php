5d2e1bf848674acbe675ab5b8894a143d562a74d
alexialang <alexialang2@gmail.com>
2024-04-24 15:46:50 +0200

    material

---
+++ templates/materiels.html.php
@@ -0,0 +1,65 @@
+<div class="container-index">
+  <div class="left-section-index">
+    <!-- Menu Latéral -->
+    <div>
+      <div class="logo-section-sidebar">
+        <img src="assets/img/logo_computer_locmns.svg" alt="logo-computer" />
+        <img src="assets/img/logo_locmns.svg" alt="logo" />
+      </div>
+      <ul>
+        <li><a href="/"> <img src="assets/img/house.svg" />Accueil</a></li>
+        <li><a href="/"> <img src="assets/img/computer.svg" />Matériels</a></li>
+        <li><a href="/"> <img src="assets/img/bell.svg" />Notifications</a></li>
+        <li><a href="/"> <img src="assets/img/users.svg" />Utilisateurs</a></li>
+        <li><a href="/"> <img src="assets/img/gear.svg" />Réglages</a></li>
+      </ul>
+    </div>
+    <div><a href="/"><img src="assets/img/door.svg" />
+        Déconnexion</a>
+    </div>
+  </div>
+  <div class="rigth-section-index">
+    <!-- Nav -->
+    <div class="nav-bar">
+      <h1>Matériels</h1>
+      <div>
+        <input class="input-search" type="text" /></input>
+        <img class="search" src="assets/img/magnifier.svg" />
+        <img src="assets/img/user.svg" />
+        <img src="assets/img/chevron-down.svg" />
+      </div>
+    </div>
+    <!-- MAIN -->
+    <div class="container-main-material">
+      <div class="top-container-material"></div>
+      <div class="container-material">
+        <div><a href="/">Gérer <img src="assets/img/circles.svg" alt="logo gérer" /></a></div>
+        <div class="container-cards-material">
+        <div class="card-material">
+          <div></div>
+          <div>
+
+            <h3>Tous les équiments</h3> <button>Voir <img src="assets/img/chevron-white.svg"></button>
+          </div>
+        </div>
+        <div class="card-material">
+          <div class="img-card-material"></div>
+          <div>
+            <h3>Catégories</h3><button>Voir <img src="assets/img/chevron-white.svg"></button>
+          </div>
+        </div>
+        <div class="card-material">
+          <div></div>
+          <div>
+            <h3>Marques</h3><button>Voir <img src="assets/img/chevron-white.svg"></button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+</div>
