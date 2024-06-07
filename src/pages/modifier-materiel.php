1fb2791c80ea5481eb9e5f2edc5a85a472864ef4
alexialang <alexialang2@gmail.com>
2024-06-07 14:46:43 +0200

    modification materiel

--- src/pages/modifier-materiel.php
+++ src/pages/modifier-materiel.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if (empty($_SESSION['user_id'])) {
     header('Location: /?page=connexion');
     exit;
@@ -16,7 +15,11 @@ if (!empty($_GET['id'])) {
     $_POST['material']['id_material'] = $_GET['id'];
 
     // Préparer la requête avec un paramètre :id
-    $query = $dbh->prepare("SELECT * FROM material WHERE id_material = :id");
+    $query = $dbh->prepare("SELECT * FROM material 
+                             LEFT JOIN category ON category.id_category = material.id_category 
+                            LEFT JOIN model ON model.id_model = category.id_category 
+                LEFT JOIN brand ON brand.id_brand = category.id_category
+                            WHERE id_material = :id");
 
     // Exécuter la requête en liant la valeur de :id à l'ID récupéré de l'URL
     $query->execute(['id' => $_GET['id']]);
@@ -38,25 +41,77 @@ if (!empty($_GET['id'])) {
             $errors['material']['description'] = "Veuillez saisir une description, qui contient plus d'un caractère.";
         }
 
-        //Validation du champs "Numéros de serie"
-        if (empty($_POST['material']['serial_number']|| strlen($_POST['material']['description']) <= 1)) {
-            $errors['material']['serial_number'] = "Veuillez sélectionner un numéro de série, qui contien plus d'un caractère.";
+        // Validation du champs "Numéros de série"
+        if (empty($_POST['material']['serial_number']) || strlen($_POST['material']['serial_number']) <= 1) {
+            $errors['material']['serial_number'] = "Veuillez sélectionner un numéro de série, qui contient plus d'un caractère.";
         }
 
-        // Validation du champs "Email"
-        if (empty($_POST['material']['email']) || !filter_var($_POST['material']['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors['material']['email'] = "Veuillez saisir un email valide.";
+        // Validation du champs "Date d'achat"
+        if (empty($_POST['material']['date_purchase'])) {
+            $errors['material']['date_purchase'] = "Veuillez saisir une date d'achat valide.";
         }
 
-        // Si la variable erreurs est vide 
+        // Validation du champs "Marque"
+        if (empty($_POST['material']['name_brand']) || strlen($_POST['material']['name_brand']) <= 1) {
+            $errors['material']['name_brand'] = "Veuillez saisir une marque, qui contient plus d'un caractère.";
+        }
+
+        // Validation du champs "Dimension"
+        if (empty($_POST['material']['screen_size']) || strlen($_POST['material']['screen_size']) <= 1) {
+            $errors['material']['screen_size'] = "Veuillez saisir une dimension valide.";
+        }
+
+        // Validation du champs "Catégorie"
+        if (empty($_POST['material']['name_category']) || strlen($_POST['material']['name_category']) <= 1) {
+            $errors['material']['name_category'] = "Veuillez saisir une catégorie valide.";
+        }
+
+        // Validation des champs spécifiques aux Laptops/Tablettes
+        if ($_POST['material']['name_category'] === 'Laptop' || $_POST['material']['name_category'] === 'Tablette') {
+            if (empty($_POST['material']['processor']) || strlen($_POST['material']['processor']) <= 1) {
+                $errors['material']['processor'] = "Veuillez saisir un processeur valide.";
+            }
+            if (empty($_POST['material']['storage_memory']) || strlen($_POST['material']['storage_memory']) <= 1) {
+                $errors['material']['storage_memory'] = "Veuillez saisir une mémoire valide.";
+            }
+            if (empty($_POST['material']['ram']) || strlen($_POST['material']['ram']) <= 1) {
+                $errors['material']['ram'] = "Veuillez saisir une RAM valide.";
+            }
+        }
+
+        // Si la variable erreurs est vide
         if (empty($errors)) {
             // Effectuer les modifications dans la base de données
-            $query = $dbh->prepare("UPDATE material SET name_material = :name_material, description = :description, serial_number = :serial_number WHERE id_material = :id_material");
-            $query->execute($_POST['material']);
+            $query = $dbh->prepare("UPDATE material SET 
+                name_material = :name_material, 
+                description = :description, 
+                serial_number = :serial_number,
+                date_purchase = :date_purchase,
+                screen_size = :screen_size,
+                processor = :processor,
+                storage_memory = :storage_memory,
+                ram = :ram
+                WHERE id_material = :id_material");
+
+            // Passer les paramètres au tableau
+            $query->execute([
+                'name_material' => $_POST['material']['name_material'],
+                'description' => $_POST['material']['description'],
+                'serial_number' => $_POST['material']['serial_number'],
+                'date_purchase' => $_POST['material']['date_purchase'],
+                'screen_size' => $_POST['material']['screen_size'],
+                'processor' => $_POST['material']['processor'] ?? null,
+                'storage_memory' => $_POST['material']['storage_memory'] ?? null,
+                'ram' => $_POST['material']['ram'] ?? null,
+                'id_material' => $_POST['material']['id_material']
+            ]);
 
             // Vérifier si la mise à jour a réussi
             if ($query->rowCount() > 0) {
+                
                 $success = "Les modifications de l'utilisateur ont été enregistrées avec succès.";
+                header('Location: /?page=ensemble-materiel');
+                exit;
             } else {
                 $errors['form'] = "Une erreur s'est produite lors de la modification de l'utilisateur. Contactez l'administrateur à l'adresse [email].";
             }
--- templates/modifier-materiel.html.php
+++ templates/modifier-materiel.html.php
@@ -1,73 +1,133 @@
  <div class="container-main-material">
    <!-- BARRE GRISE -->
-   <div class="top-container-material"> <a href="/?page=ensemble-materiel"><img src="./assets/img/arrow-return.svg"></a> <div class="top-txt-container-material"><a href="/?page=materiels">Matériels </a> / <a href="/?page=ensemble-materiel"> Tous les équipements </a>/ Modifier un matériel</div></div>
+   <div class="top-container-material"> <a href="/?page=ensemble-materiel"><img src="./assets/img/arrow-return.svg"></a>
+     <div class="top-txt-container-material"><a href="/?page=materiels">Matériels </a> / <a href="/?page=ensemble-materiel"> Tous les équipements </a>/ Modifier un matériel</div>
+   </div>
    <!-- CONTAINER GLOBAL -->
    <div class="container-material">
      <div class="flex-container">
 
        <div class="container-form-inscription">
          <form class="form-update-materiel" method="POST">
+
            <div>
-             <label for="name_material">Nom :</label>
-             <input type="text" name="material[name_material]" id="lastname_update" value="<?= $material['name_material'] ?>" placeholder="Nom" required>
-             <!--Message d'erreur -->
-             <?php if (isset($errors) && !empty($errors['material']['name_material'])) : ?>
+
+             <div>
+               <label for="name_material">Nom :</label>
+               <input type="text" name="material[name_material]" id="lastname_update" value="<?= $material['name_material'] ?>" placeholder="Nom" required>
+               <!--Message d'erreur -->
+               <?php if (isset($errors) && !empty($errors['material']['name_material'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['name_material'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <div class="flex">
+               <label for="description">Description :</label>
+               <textarea class="border-none" name="material[description]" id="firstname_update" placeholder="Description" rows="5" cols="40" required><?= $material['description'] ?></textarea>
+               <?php if (isset($errors) && !empty($errors['material']['description'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['description'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <div>
+               <label for="serial_number">Numéro de série:</label>
+               <input type="text" name="material[serial_number]" id="serialNumber" value="<?= $material['serial_number'] ?>" placeholder="Numéro de série" required>
+               <?php if (isset($errors) && !empty($errors['material']['serial_number'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['serial_number'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <div>
+               <label for="date_purchase">Date achat :</label>
+               <input type="date" name="material[date_purchase]" id="datePurchase" value="<?= $material['date_purchase'] ?>" placeholder="Date d'achat" required>
+               <?php if (isset($errors) && !empty($errors['material']['date_purchase'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['date_purchase'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <div>
+               <label for="name_brand">Marque :</label>
+               <input type="text" name="material[name_brand]" id="datePurchase" value="<?= $material['name_brand'] ?>" placeholder="Date d'achat" >
+               <?php if (isset($errors) && !empty($errors['material']['name_brand'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['name_brand'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <div>
+               <label for="date_purchase">Dimension :</label>
+               <input type="text" name="material[screen_size]" id="datePurchase" value="<?= $material['screen_size'] ?>" placeholder="Date d'achat" required>
+               <?php if (isset($errors) && !empty($errors['material']['screen_size'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['screen_size'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <label for="name_category">Catégorie :</label>
+             <input type="text" name="material[name_category]" id="datePurchase" value="<?= $material['name_category'] ?>" placeholder="Date d'achat" required>
+             <?php if (isset($errors) && !empty($errors['material']['name_category'])) : ?>
                <div class="errors-inscr">
-                 <?= $errors['material']['name_material'] ?>
-               </div>
-             <?php endif; ?>
-           </div>
-           <div>
-             <label for="description">Description :</label>
-             <input type="text" name="material[description]" id="firstname_update" value="<?= $material['description'] ?>" placeholder="Description" required>
-             <?php if (isset($errors) && !empty($errors['material']['description'])) : ?>
-               <div class="errors-inscr">
-                 <?= $errors['material']['description'] ?>
-               </div>
-             <?php endif; ?>
-           </div>
-           <div>
-             <label for="serial_number">Numéro de série:</label>
-             <input type="text" name="material[serial_number]" id="serialNumber" value="<?= $material['serial_number'] ?>" placeholder="Numéro de série" required>
-             <?php if (isset($errors) && !empty($errors['material']['serial_number'])) : ?>
-               <div class="errors-inscr">
-                 <?= $errors['material']['serial_number'] ?>
+                 <?= $errors['material']['name_category'] ?>
                </div>
              <?php endif; ?>
            </div>
 
+           <?php if ($material['name_category'] === 'Laptop' || $material['name_category'] === 'Tablette') : ?>
+
+             <div>
+               <label for="processor">Processeur :</label>
+               <input type="text" name="material[processor]" id="datePurchase" value="<?= $material['processor'] ?>" placeholder="Date d'achat" required>
+               <?php if (isset($errors) && !empty($errors['material']['processor'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['processor'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <div>
+               <label for="storage_memory">Mémoire :</label>
+               <input type="text" name="material[storage_memory]" id="datePurchase" value="<?= $material['storage_memory'] ?>" placeholder="Date d'achat" required>
+               <?php if (isset($errors) && !empty($errors['material']['storage_memory'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['storage_memory'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+
+             <div>
+               <label for="ram">RAM :</label>
+               <input type="text" name="material[ram]" id="datePurchase" value="<?= $material['ram'] ?>" placeholder="Date d'achat" required>
+               <?php if (isset($errors) && !empty($errors['material']['ram'])) : ?>
+                 <div class="errors-inscr">
+                   <?= $errors['material']['ram'] ?>
+                 </div>
+               <?php endif; ?>
+             </div>
+           <?php endif; ?>
 
            <div>
-             <label for="date_purchase">Date achat :</label>
-             <input type="date" name="material[date_purchase]" id="datePurchase" value="<?= $material['date_purchase'] ?>" placeholder="Date d'achat" required>
-             <?php if (isset($errors) && !empty($errors['material']['date_purchase'])) : ?>
-               <div class="errors-inscr">
-                 <?= $errors['material']['date_purchase'] ?>
-               </div>
-             <?php endif; ?>
-           </div>
-
-           <div>
-             <label for="date_purchase">Date achat :</label>
-             <input type="date" name="material[date_purchase]" id="datePurchase" value="<?= $material['date_purchase'] ?>" placeholder="Date d'achat" required>
-             <?php if (isset($errors) && !empty($errors['material']['date_purchase'])) : ?>
-               <div class="errors-inscr">
-                 <?= $errors['material']['date_purchase'] ?>
-               </div>
-             <?php endif; ?>
-           </div>
-           <div class="submit-inscription">
-             <input type="submit" name="submit" value="Modifier">
-             <?php if (isset($success) && !empty($success)) : ?>
-               <div class="success-inscr">
-                 <?= $success ?>
-               </div>
-             <?php endif; ?>
-           </div>
-
+             <div class="submit-inscription">
+               <input type="submit" name="submit" value="Modifier">
+               <?php if (isset($success) && !empty($success)) : ?>
+                 <div class="success-inscr">
+                   <?= $success ?>
+                 </div>
+               <?php endif; ?>
+             </div>
          </form>
        </div>
-       <div > <img src="./assets/img/update-material.svg" alt=""></div>
      </div>
+     <div class="img-div"> <img src="./assets/img/update-material.svg" alt=""></div>
    </div>
+ </div>
  </div>
